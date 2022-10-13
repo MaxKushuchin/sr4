5c82419754f6cb95100cd10a64773ed25dc565b1
MaxKushuchin <114504964+MaxKushuchin@users.noreply.github.com>
2022-10-13 10:03:20 +0300

    1.py
    1 задание

--- 1.py
+++ 1.py
@@ -1,9 +1,9 @@
-n = int(input())
-array = [float(i) for i in input().split()]
+n = int(input()) #размерность массива
+array = [float(i) for i in input().split()] #ввод массива с вещественными значениями в строку
 mx = 0
 for i in range(n):
     if (array[i] >= array[mx]):
-        mx = i
+        mx = i 
 for i in range (mx + 1, n):
     array[i] = 0
 print(array)
